e09c9daf1d28b106992ae984a9db6bc1136ccc5a
Isaiah <isaiahsosa@gmail.com>
2019-02-16 12:49:33 -0500

    Website
    First intial upload of website.

---
+++ portfolio/js/script.js
@@ -0,0 +1,90 @@
+$(document).ready(function(){
+    $('#slides').superslides({
+        animation:  'fade',
+        play: 7000,
+        pagination:  false
+    });
+
+    var typed = new Typed(".typed", {
+        strings: ["Web Developer.", "UI/UX Designer.", "Programmer."],
+        typeSpeed: 70,
+        loop: true,
+        startDelay: 1000,
+        showCursor: false
+    });
+
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        items: 4,
+        responsive: {
+            0: {
+                items: 1
+            },
+            480: {
+                items: 2
+            },
+            768: {
+                items: 2
+            },
+            938: {
+                items: 4
+            }
+
+        }
+    });
+
+
+    var skillsTopOffset = $(".skillsSection").offset().top;
+    
+
+    $(window).scroll(function() {
+        if(window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
+
+            $('.chart').easyPieChart({
+                easing: 'easeInOut',
+                barColor: '#fff',
+                trackColor: false,
+                scaleColor: false,
+                lineWidth: 4,
+                size: 152,
+                onStep: function (from, to, percent) {
+                    $(this.el).find('.percent').text(Math.round(percent));
+                }
+            });
+
+        }
+    });
+
+    $("[data-fancybox").fancybox();
+
+    $(".items").isotope({
+        filter: '*',
+        animationOptions: {
+            duration: 1500,
+            easing: 'linear',
+            queue: false
+        }
+    });
+
+    $("#filters a").click(function() {
+        $("#filters .current").removeClass("current");
+        $(this).addClass("current");
+
+        var selector = $(this).attr("data-filter");
+
+
+        $(".items").isotope({
+            filter: selector,
+            animationOptions: {
+                duration: 1500,
+                easing: 'linear',
+                queue: false
+        }
+    });
+
+    return false;
+
+    });
+
+});
+
